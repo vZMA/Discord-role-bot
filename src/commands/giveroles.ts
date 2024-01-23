ac0c223a5b5180b155ed7bd6da5ec967b91c5d2d
Alex <43963205+AHanadsaker@users.noreply.github.com>
2024-01-23 15:34:22 -0600

    Update giveroles.ts

--- src/commands/giveroles.ts
+++ src/commands/giveroles.ts
@@ -125,22 +125,8 @@ export class VerifyCommand extends Command {
             }
           }
           //* Edit below based on neighboring ARTCCs
-          switch (user.artcc) {
-              case "ZJX": {
-              roles.push(await interaction.guild?.roles.fetch('1199464076724682772'));
-              break;
-            }
-            case "ZHU": {
-              roles.push(await interaction.guild?.roles.fetch('1199464076724682772'));
-              break;
-            }
-            default: {
-              break;
-            }
-          }
-        } else {
           roles.push(await interaction.guild?.roles.fetch(config.member)); //Add 'ZJX Controller' role
-        }
+          
       } catch (error) {
         await handleError(0, interaction);
         console.log(`Line 115: ${error}`);
